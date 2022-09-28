85d066e38b9086d10b9fa8ffb4d1022c84a12834
Dillon <dillonmorris91@gmail.com>
2022-09-27 18:25:30 -0700

    Updated photos and words

--- src/pages/about.jsx
+++ src/pages/about.jsx
@@ -61,58 +61,69 @@ export default function About() {
           </div>
           <div className="lg:order-first lg:row-span-2">
             <h1 className="text-4xl font-bold tracking-tight text-zinc-800 dark:text-zinc-100 sm:text-5xl">
-              I’m Spencer Sharp. I live in New York City, where I design the
-              future.
+              I’m Dillon. I build apps with React and Typescript.
             </h1>
             <div className="mt-6 space-y-7 text-base text-zinc-600 dark:text-zinc-400">
               <p>
-                I’ve loved making things for as long as I can remember, and
-                wrote my first program when I was 6 years old, just two weeks
-                after my mom brought home the brand new Macintosh LC 550 that I
-                taught myself to type on.
+                I've always had diverse interests. I love sports. As a kid I was
+                obsessed with basketball and recently got back into it, playing
+                pick-up games at my gym. I’m also a huge golf nerd and try to
+                get out and play as much as I can. I’ve always had a passion for
+                music and have played the guitar since the 6th grade (it doesn’t
+                show). With these and other diverse interests I'm able to find
+                common ground with anyone and see things from different
+                perspectives.
               </p>
               <p>
-                The only thing I loved more than computers as a kid was space.
-                When I was 8, I climbed the 40-foot oak tree at the back of our
-                yard while wearing my older sister’s motorcycle helmet, counted
-                down from three, and jumped — hoping the tree was tall enough
-                that with just a bit of momentum I’d be able to get to orbit.
+                I’m a former customer service professional and have ingrained
+                empathy for end users. I started in tech because that’s where I
+                felt I could have the biggest impact helping people. I love this
+                field because there is something new to learn every single day.
+                I try not to make too many assumptions based on previous
+                knowledge. You are never too good to go back to the drawing
+                board, to see things in a new light.
               </p>
               <p>
-                I spent the next few summers indoors working on a rocket design,
-                while I recovered from the multiple surgeries it took to fix my
-                badly broken legs. It took nine iterations, but when I was 15 I
-                sent my dad’s Blackberry into orbit and was able to transmit a
-                photo back down to our family computer from space.
-              </p>
-              <p>
-                Today, I’m the founder of Planetaria, where we’re working on
-                civilian space suits and manned shuttle kits you can assemble at
-                home so that the next generation of kids really <em>can</em>{' '}
-                make it to orbit — from the comfort of their own backyards.
+                I am a developer specializing in React, Next and Typescript. I
+                also have experience in React Native, having worked extensively
+                on a fitness app at my previous company, Synapse Studios.
+                Currently, I get to wield and grow these skills at a company
+                called Formidable. Right now I’m part of a team helping a big
+                name clothing brand migrate to React. I love being part of a
+                team and take pride in helping others level up while learning
+                from those who can help me fill my own knowledge gaps.
               </p>
             </div>
           </div>
           <div className="lg:pl-20">
             <ul role="list">
-              <SocialLink href="#" icon={TwitterIcon}>
-                Follow on Twitter
-              </SocialLink>
-              <SocialLink href="#" icon={InstagramIcon} className="mt-4">
-                Follow on Instagram
-              </SocialLink>
-              <SocialLink href="#" icon={GitHubIcon} className="mt-4">
+              <SocialLink
+                href="https://github.com/dillonemorris"
+                icon={GitHubIcon}
+                className="mt-4"
+              >
                 Follow on GitHub
               </SocialLink>
-              <SocialLink href="#" icon={LinkedInIcon} className="mt-4">
+              <SocialLink
+                href="https://www.linkedin.com/in/dillonmorrisdev/"
+                icon={LinkedInIcon}
+                className="mt-4"
+              >
                 Follow on LinkedIn
               </SocialLink>
               <SocialLink
-                href="mailto:spencer@planetaria.tech"
+                href="https://www.instagram.com/dillonemorris/"
+                icon={InstagramIcon}
+                className="mt-4"
+              >
+                Follow on Instagram
+              </SocialLink>
+              <SocialLink
+                href="mailto:dillonmorris91@gmail.com"
                 icon={MailIcon}
                 className="mt-8 border-t border-zinc-100 pt-8 dark:border-zinc-700/40"
               >
-                spencer@planetaria.tech
+                dillonmorris91@gmail.com
               </SocialLink>
             </ul>
           </div>
--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -251,7 +251,7 @@ export default function Home({ articles }) {
     <>
       <Head>
         <title>
-          Dillon Morris - Software engineer, designer, and React expert
+          Dillon Morris - Software engineer, designer, and React connoisseur
         </title>
         <meta
           name="description"
@@ -261,7 +261,7 @@ export default function Home({ articles }) {
       <Container className="mt-9">
         <div className="max-w-2xl">
           <h1 className="text-4xl font-bold tracking-tight text-zinc-800 dark:text-zinc-100 sm:text-5xl">
-            Software engineer, designer, and React expert.
+            Software engineer, designer, and React connoisseur.
           </h1>
           <p className="mt-6 text-base text-zinc-600 dark:text-zinc-400">
             I’m Dillon, a software engineer and designer based in Phoenix.
