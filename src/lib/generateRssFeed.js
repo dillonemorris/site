908cf3ccd9c68d737174ac8d31bb41246ef7a00e
Dillon <dillonmorris91@gmail.com>
2022-12-28 16:37:56 -0700

    Publish music blog post

--- src/lib/generateRssFeed.js
+++ src/lib/generateRssFeed.js
@@ -9,7 +9,7 @@ export async function generateRssFeed() {
   let siteUrl = process.env.NEXT_PUBLIC_SITE_URL
   let author = {
     name: 'Dillon Morris',
-    email: 'spencer@planetaria.tech',
+    email: 'dillonmorris91@gmail.com',
   }
 
   let feed = new Feed({
