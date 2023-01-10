2f922fb8d2685d76716a31a22350059800b1dabf
Dillon <dillonmorris91@gmail.com>
2023-01-10 10:10:42 -0700

    Image resizing for perf

--- src/pages/about.jsx
+++ src/pages/about.jsx
@@ -9,7 +9,7 @@ import {
   GitHubIcon,
   LinkedInIcon,
 } from '@/components/SocialIcons'
-import portraitImage from '@/images/portrait.jpg'
+import portraitImage from '@/images/photos/portrait.jpg'
 
 function SocialLink({ className, href, children, icon: Icon }) {
   return (
@@ -52,7 +52,7 @@ export default function About() {
             <div className="max-w-xs px-2.5 lg:max-w-none">
               <Image
                 src={portraitImage}
-                alt=""
+                alt="Dillon posing with golden doodle dog"
                 sizes="(min-width: 1024px) 32rem, 20rem"
                 className="aspect-square rotate-3 rounded-2xl bg-zinc-100 object-cover dark:bg-zinc-800"
               />
