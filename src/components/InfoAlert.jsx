4eb962d990e6566febf9be16b97419f4c6d993c3
Dillon <dillonmorris91@gmail.com>
2023-05-12 16:54:11 -0700

    Add remaining content and images

---
+++ src/components/InfoAlert.jsx
@@ -0,0 +1,19 @@
+import { InformationCircleIcon } from '@heroicons/react/20/solid'
+
+export function InfoAlert({ children }) {
+  return (
+    <div className="rounded-md border-l-4 border-blue-400 bg-blue-50 p-4">
+      <div className="flex">
+        <div className="flex-shrink-0">
+          <InformationCircleIcon
+            className="h-6 w-6 text-blue-400"
+            aria-hidden="true"
+          />
+        </div>
+        <div className="mx-4 flex-1 md:flex md:justify-between">
+          <p className="text-md my-0 text-gray-900">{children}</p>
+        </div>
+      </div>
+    </div>
+  )
+}
